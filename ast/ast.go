7b9aac82750bbf43ca913171917dc7a3f1309693
solbero <njord.solberg@gmail.com>
2023-07-17 15:54:23 +0200

    fixup! Add string method to node interface

--- ast/ast.go
+++ ast/ast.go
@@ -61,9 +61,9 @@ func (ls *LetStatement) String() string {
 	out.WriteString(ls.Name.String())
 	out.WriteString(" = ")
 
-	// if ls.Value != nil {
-	// 	out.WriteString(ls.Value.String())
-	// }
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
 
 	out.WriteString(";")
 
