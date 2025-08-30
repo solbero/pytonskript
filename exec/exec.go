598f0341ac24486bd1602c61ac12be64f6b43d49
solbero <njosol@pm.me>
2025-08-30 10:49:13 +0000

    Implement file execution

---
+++ exec/exec.go
@@ -0,0 +1,58 @@
+package exec
+
+import (
+	"io"
+
+	"github.com/solbero/monkey/evaluator"
+	"github.com/solbero/monkey/lexer"
+	"github.com/solbero/monkey/object"
+	"github.com/solbero/monkey/parser"
+)
+
+func Start(in io.Reader, out io.Writer) {
+	env := object.NewEnvironment()
+	bytes, err := readContents(in)
+	if err != nil {
+		panic(err)
+	}
+
+	s := string(bytes)
+	l := lexer.New(s)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		printParserErrors(out, p.Errors())
+		return
+	}
+
+	evaluator.Eval(program, env)
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	// io.WriteString(out, MONKEY_FACE)
+	// io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+func readContents(f io.Reader) ([]byte, error) {
+	data := make([]byte, 0, 1024)
+	for {
+		n, err := f.Read(data[len(data):cap(data)])
+		data = data[:len(data)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return data, err
+		}
+
+		if len(data) >= cap(data) {
+			d := append(data[:cap(data)], 0)
+			data = d[:len(data)]
+		}
+	}
+}
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/solbero/monkey/repl"
 	"os"
 	"os/user"
+
+	"github.com/solbero/monkey/exec"
+	"github.com/solbero/monkey/repl"
 )
 
 func main() {
@@ -14,7 +16,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+
+	args := os.Args
+
+	switch len(args) {
+	case 1:
+		fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+	case 2:
+		file, err := os.Open(args[1])
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		exec.Start(file, os.Stdout)
+	default:
+		fmt.Fprintf(os.Stderr, "%q: incorrect usage: Usage: `monkey [filePath]`\n", os.Args[0])
+	}
+
 }
